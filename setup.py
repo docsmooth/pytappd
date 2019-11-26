ca2f471b4c0f6ea50f76bfe38917fd9b44851aff
Robert Auch <rauch@totalnetsolutions.net>
2019-11-25 20:24:48 -0600

    fix up setup tool

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python
 
-from distutils.core import setup
+from setuptools import setup,find_packages
 
 setup(
     name='PyTappd',
     version='0.4',
-    packages=['pytappd',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    author='Robert Auch',
+    author_email='rauch@totalnetsolutions.net',
     long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Indended Audience :: Security Administrators',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.7',
+        ],
+    package_dir={'':'src'},
+    packages=find_packages(where='src'),
+    python_requires='>=2.7',
+    entry_points={
+        'console_scripts': [
+            'sample=sample:main',
+        ],
+    },
 )
