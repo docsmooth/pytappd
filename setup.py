19cf2807c5420254e46ced141d57ddb42ada0a9f
Robert Auch <rauch@totalnetsolutions.net>
2021-11-08 22:20:21 -0600

    Setting up new BILU client

--- setup.py
+++ setup.py
@@ -3,21 +3,20 @@
 from setuptools import setup,find_packages
 
 setup(
-    name='PyTappd',
-    version='0.7',
+    name='BIUL',
+    version='0.1',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     author='Robert Auch',
-    author_email='rauch@totalnetsolutions.net',
+    author_email='rauch@beyondtrust.com',
     long_description=open('README.md').read(),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Indended Audience :: Security Administrators',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.7',
         ],
     package_dir={'':'src'},
     packages=find_packages(where='src'),
-    python_requires='>=2.7',
+    python_requires='>=3.6',
     install_requires=[
         'requests',
         'cmd',
